11ebdb450cd0c9a500e86bc66a6e7f6c51955828
arikoff <arikov@mail.ru>
2022-07-19 09:24:50 +0300

    O1

---
+++ sum.js
@@ -0,0 +1,18 @@
+function sum(x) {
+
+    if (x === undefined) {
+        return 0;
+    }
+
+    let s = x;
+
+    function f(b) {
+        if (b === undefined) {
+            return s;
+        }
+        s += b;
+        return f;   
+    }
+    return f;
+  };
+
